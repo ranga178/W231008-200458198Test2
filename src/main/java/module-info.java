903f3525de9ddbc927db97e713ab259eaea6c98c
Aaron Girard <113151264+ranga178@users.noreply.github.com>
2023-03-06 13:12:29 -0500

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.w231008200458198test2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.w231008200458198test2 to javafx.fxml;
+    exports com.example.w231008200458198test2;
+}
