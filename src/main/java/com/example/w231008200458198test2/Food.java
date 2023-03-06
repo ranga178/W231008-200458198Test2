250d1b4c6b42bbc94bf76f1b4c5c4e3e2ee88b16
Aaron Girard <113151264+ranga178@users.noreply.github.com>
2023-03-06 13:20:00 -0500

    question 2 complete

---
+++ src/main/java/com/example/w231008200458198test2/Food.java
@@ -0,0 +1,8 @@
+package com.example.w231008200458198test2;
+
+public class Food {
+    private String name;
+    private int calories;
+    private double price;
+    private int protein;
+}
---
+++ src/main/java/com/example/w231008200458198test2/Meal.java
@@ -0,0 +1,7 @@
+package com.example.w231008200458198test2;
+
+import java.util.ArrayList;
+
+public class Meal {
+    private ArrayList<Food> cart;
+}
